9ea69a711a041ebde32302f4b2f8d5168b42347c
abhishek.srivastava@ <abhishek.srivastava@neudesic.com>
2023-07-12 19:43:12 +0530

    blob upload using script is tested

--- filemanager/src/blobmanager/blobhandler.py
+++ filemanager/src/blobmanager/blobhandler.py
@@ -7,27 +7,51 @@ from blobmanager.blobconfig import AzureBlobConfig
 from azure.identity import DefaultAzureCredential
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 
+import os
+import uuid
 import logging
-
-logging.basicConfig(level=logging.DEBUG, filename="output.log")
 
 class AzureBlobHandler():
 
     def __init__(self, config: AzureBlobConfig):
         self.config = config
 
-    def get_blob_service_client_sas(self):
-        account_url = self.config.account_url
-        credential = self.config.token
-        # Create the BlobServiceClient object
-        blob_service_client = BlobServiceClient(account_url, credential=credential)
+    # def get_blob_service_client_sas(self):
+    #     account_url = self.config.account_url
+    #     credential = self.config.token
+    #     # Create the BlobServiceClient object
+    #     blob_service_client = BlobServiceClient(account_url, credential=credential)
+    #     return blob_service_client
+    def get_blob_service_client(self):
+        blob_service_client = BlobServiceClient.from_connection_string(self.config.connectionString)
         return blob_service_client
     
-    def list_containers(self):
-        i=0
-        blob_service_client = self.get_blob_service_client_sas()
+    def get_list_containers(self):
+        blob_service_client = self.get_blob_service_client()
         containers = blob_service_client.list_containers(include_metadata=True)
-        return containers
+        result = []
+        for container in containers:
+             result.append(container['name'])
+        return result
+    
+    def get_list_blob(self, containerName: str):
+        blob_service_client = self.get_blob_service_client()
+        container_client = blob_service_client.get_container_client(containerName)
+        names = []
+        blob_list = container_client.list_blobs()
+        for blob in blob_list:
+            names.append(blob.name)
+        return names
+     
     
     def uploadFile(self, filePath: str):
         logging.debug("Upload file method inside blob manager is called")
+        blob_service_client = self.get_blob_service_client()
+        container_client = blob_service_client.get_container_client(container=self.config.containername)
+        with open(filePath, mode="rb") as data:
+            blobname = str(uuid.uuid4())
+            extension = os.path.splitext(filePath)[1]
+            name = blobname + extension
+            blob_client = container_client.upload_blob(name=name, data=data, overwrite=True)
+        logging.debug("Upload file completed")
+
--- filemanager/src/uploader/fileuploader.py
+++ filemanager/src/uploader/fileuploader.py
@@ -7,19 +7,74 @@ from blobmanager.blobhandler import AzureBlobConfig, AzureBlobHandler
 import json
 import logging
 
-logging.basicConfig(level=logging.DEBUG, filename="output.log")
+#logging.basicConfig(level=logging.DEBUG, filename="output.log")
+logging.basicConfig(level=logging.DEBUG)
+
+# def getAzureStorageAccountConfiguration():
+#     try:
+#         with open("config.json") as file:
+#             config = json.load(file)
+#             blob_credential = config["azure_storage_account"]
+#         blobConfig = AzureBlobConfig(blob_credential["maxfilesize"], blob_credential["account_url"], blob_credential["token"], blob_credential["container_name"])
+#         return blobConfig
+#     except Exception as err:
+#         logging.error(f"Exception Type  - {type(err)}")
+#         logging.error(f"Exception arguments  - {err.args}")
+#         logging.error(f"Exception details  - {err}")
+#         raise Exception(err)
 
 def getAzureStorageAccountConfiguration():
-    with open("config.json") as file:
-        config = json.load(file)
-        blob_credential = config["azure_storage_account"]
-    blobConfig = AzureBlobConfig(blob_credential["maxfilesize"], blob_credential["account_url"], blob_credential["token"])
-    return blobConfig
+    try:
+        with open("config.json") as file:
+            config = json.load(file)
+            blob_credential = config["azure_storage_account"]
+        blobConfig = AzureBlobConfig(blob_credential["account_name"], blob_credential["account_key"], blob_credential["connectionstring"], blob_credential["container_name"])
+        return blobConfig
+    except Exception as err:
+        logging.error(f"Exception Type  - {type(err)}")
+        logging.error(f"Exception arguments  - {err.args}")
+        logging.error(f"Exception details  - {err}")
+        raise Exception(err)
+
 
 def uploadfile(filePath: str):
-    logging.debug("Upload file method is called")
-    blobConfig = getAzureStorageAccountConfiguration()
-    blobmanager = AzureBlobHandler(blobConfig)
-    blobmanager.uploadFile(filePath)
+    try:
+        logging.debug("Upload file method is called")
+        blobConfig = getAzureStorageAccountConfiguration()
+        blobmanager = AzureBlobHandler(blobConfig)
+        blobmanager.uploadFile(filePath)
+    except Exception as err:
+        logging.error(f"Exception Type  - {type(err)}")
+        logging.error(f"Exception arguments  - {err.args}")
+        logging.error(f"Exception details  - {err}")
+        raise Exception(err)
     
-uploadfile("config.json")
+def getContianerDetails():
+    try:
+        logging.debug("getContianerDetails method is called")
+        blobConfig = getAzureStorageAccountConfiguration()
+        blobmanager = AzureBlobHandler(blobConfig)
+        data = blobmanager.list_containers()
+        logging.debug(f"Container list {data}")
+    except Exception as err:
+        logging.error(f"Exception Type  - {type(err)}")
+        logging.error(f"Exception arguments  - {err.args}")
+        logging.error(f"Exception details  - {err}")
+        raise Exception(err)
+    
+def getListOfBlobs():
+    try:
+        logging.debug("getListOfBlobs method is called")
+        blobConfig = getAzureStorageAccountConfiguration()
+        blobmanager = AzureBlobHandler(blobConfig)
+        data = blobmanager.get_list_blob(blobConfig.containername)
+        logging.debug(f"blob list {data}")
+    except Exception as err:
+        logging.error(f"Exception Type  - {type(err)}")
+        logging.error(f"Exception arguments  - {err.args}")
+        logging.error(f"Exception details  - {err}")
+        raise Exception(err)
+    
+#getContianerDetails()
+uploadfile(r"C:\Users\Abhishek.Srivastava\Desktop\src\GoF_Book.pdf")
+getListOfBlobs()
